946cb870e2a14f8c3df70725bea470c1e59fc27c
Rohit Prajapati <rohitpjpti18@gmail.com>
2022-06-26 09:28:06 +0530

    configservice updated

--- src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
+++ src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -1,5 +1,4 @@
-package com.enlighten.configserver;
-
+package com.thoughtmechanix.confsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
